0135c1ff9e2ea32a2d7b42056fc35d6a1b720fbb
chuan1983 <chuan1983126@gmail.com>
2016-09-13 09:32:10 +0800

    v2

--- app/src/main/java/org/iii/tw/threadtest/MainActivity.java
+++ app/src/main/java/org/iii/tw/threadtest/MainActivity.java
@@ -2,12 +2,32 @@ package org.iii.tw.threadtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.test_view);
+    }
+    public void testBu1(View v){
+
+    }
+    public void testBu2(View v){
+
+    }
+    private class Thread1 extends Thread{
+        @Override
+        public void run() {
+            for(int i=0; i<10; i++){
+                Log.d("bread","i="+i);
+            }
+        }
     }
 }
